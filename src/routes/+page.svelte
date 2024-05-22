4d6c02e2e19f81ff305b262d25156c5b60bfad24
lucianchickenlover <avoidall99@outlook.com>
2024-05-22 22:30:54 +0200

    XD2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
 
 <main class="layout h-screen w-full bg-black py-12 md:py-24 lg:py-32 xl:py-48">
 	<section class="container px-4 md:px-6">
-		<img src="/favicon.png" alt="SvelteKit logo" class="mx-auto mb-6 max-w-[100px] md:max-w-full" />
+		
 
 		<div class="grid items-center gap-6">
 			<div class="flex flex-col justify-center space-y-4 text-center">
@@ -39,115 +39,18 @@
 					<h1
 						class="bg-gradient-to-r from-white to-gray-500 bg-clip-text text-3xl font-bold tracking-tighter text-transparent sm:text-5xl xl:text-6xl/none"
 					>
-						SvelteKit Starter Template
+						yoursource
 					</h1>
 
-					<p class="mx-auto max-w-[600px] text-zinc-200 md:text-xl">
-						A SvelteKit template with TypeScript, TailwindCSS, Lucide Icons and pre-configured with
-						ESLint, Prettier and Husky git hooks.
-					</p>
+					
 				</div>
 
-				<div
-					class="relative mx-auto rounded-xl border border-zinc-800 p-1 text-zinc-200 shadow-md transition-shadow duration-300 hover:border-zinc-700 hover:shadow-white/10"
-				>
-					<p
-						class="flex w-full cursor-pointer items-center gap-2 rounded-md bg-white/10 p-3 font-mono"
-					>
-						<span class="text-orange-500">$</span>
-
-						<span>
-							pnpx degit rajput-hemant/sveltekit-template {'<project-name>'}
-						</span>
-
-						<button
-							on:click={() => (showDropdown = !showDropdown)}
-							class="text-zinc-400 transition-colors duration-300 hover:text-white"
-						>
-							{#if isCopied}
-								<ClipboardCheck class="h-5 w-5" />
-							{:else}
-								<Clipboard class="h-5 w-5" />
-							{/if}
-						</button>
-					</p>
-
-					{#if showDropdown}
-						<div
-							class="absolute -right-16 top-8 z-10 rounded-xl border border-zinc-700 p-1 hover:border-zinc-600"
-						>
-							<ul class="sticky flex flex-col rounded-md bg-zinc-800">
-								<button
-									on:click={() => copyToClipboard('bun')}
-									class="m-0.5 w-20 cursor-pointer rounded-md px-3 py-0.5 hover:bg-zinc-700/50 hover:text-orange-500"
-								>
-									bun
-								</button>
-
-								<button
-									on:click={() => copyToClipboard('pnpm')}
-									class="m-0.5 w-20 cursor-pointer rounded-md px-3 py-0.5 hover:bg-zinc-700/50 hover:text-orange-500"
-								>
-									pnpm
-								</button>
-
-								<button
-									on:click={() => copyToClipboard('npm')}
-									class="m-0.5 w-20 cursor-pointer rounded-md px-3 py-0.5 hover:bg-zinc-700/50 hover:text-orange-500"
-								>
-									npm
-								</button>
-
-								<button
-									on:click={() => copyToClipboard('yarn')}
-									class="m-0.5 w-20 cursor-pointer rounded-md px-3 py-0.5 hover:bg-zinc-700/50 hover:text-orange-500"
-								>
-									yarn
-								</button>
-							</ul>
-						</div>
-					{/if}
-				</div>
+				
 			</div>
 		</div>
 	</section>
 
-	<footer
-		class="fixed bottom-4 mx-auto flex w-full flex-col text-center font-medium text-zinc-600 dark:text-zinc-400"
-	>
-		<div class="mx-auto flex">
-			<a
-				href="https://github.com/rajput-hemant"
-				target="_blank"
-				rel="noopener noreferrer"
-				class="rounded-md border border-transparent p-2 hover:border-zinc-700 hover:bg-white/10"
-			>
-				<Github class="h-5 w-5" />
-			</a>
-
-			<a
-				href="https://x.com/rajput_hemant01"
-				target="_blank"
-				rel="noopener noreferrer"
-				class="rounded-md border border-transparent p-2 hover:border-zinc-700 hover:bg-white/10"
-			>
-				<Twitter class="h-5 w-5" />
-			</a>
-		</div>
-
-		<span>
-			Copyright &copy; {new Date().getFullYear()}
-
-			<a
-				href="https://github.com/rajput-hemant"
-				target="_blank"
-				rel="noopener noreferrer"
-				class="bg-gradient-to-r from-white to-gray-500 bg-clip-text underline-offset-4 hover:underline"
-			>
-				rajput-hemant@github
-			</a>
-		</span>
-	</footer>
+	
 </main>
 
 <style>
